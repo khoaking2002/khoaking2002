cb068ab4a531468c70ace4b7c730e0fa606836dc
khoaking2002 <khoanguyenanh0912@gmail.com>
2022-08-23 16:43:58 +0700

    Updates

---
+++ precache-manifest.3cc46696f50d5cfc5e708fdd990a069d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "94c27889e98e8820a38a",
+    "url": "/khoaking2002/static/css/main.8bdbd6ef.chunk.css"
+  },
+  {
+    "revision": "94c27889e98e8820a38a",
+    "url": "/khoaking2002/static/js/main.94c27889.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/khoaking2002/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "ff69b631e9f71632f91a",
+    "url": "/khoaking2002/static/js/2.ff69b631.chunk.js"
+  },
+  {
+    "revision": "5554299457e1a254127a",
+    "url": "/khoaking2002/static/js/runtime~main.55542994.js"
+  },
+  {
+    "revision": "c7590874a3bb99c9b5ec409e6658b0d7",
+    "url": "/khoaking2002/static/media/khoa.c7590874.jpg"
+  },
+  {
+    "revision": "9dea3f1add263c9030dda130770472d4",
+    "url": "/khoaking2002/static/media/SVN-Astronout.9dea3f1a.otf"
+  },
+  {
+    "revision": "93309940c759cc5ce2fcebc45a1b497c",
+    "url": "/khoaking2002/static/media/SVN-Quattrocento Bold.93309940.otf"
+  },
+  {
+    "revision": "bffb928dbc625d9d6d22d1d5b6f63508",
+    "url": "/khoaking2002/index.html"
+  }
+];
